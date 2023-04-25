c5795a90aa9bed1c7c94b2cbe8e7934b70f9bb74
nof-pdx <thunderquack205@gmail.com>
2023-04-25 11:48:12 -0700

    Activity 2

---
+++ topic21/src/main/java/com/gcu/Topic21Application.java
@@ -0,0 +1,16 @@
+package com.gcu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@ComponentScan({"com.gcu"})
+public class Topic21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Topic21Application.class, args);
+	}
+
+}
